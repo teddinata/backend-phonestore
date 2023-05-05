429eb56ddbdc045a83dfc2b5fec2d6a5c05667ea
teddinata <teddinata@users.noreply.github.com>
2023-05-06 02:02:42 +0700

    fix seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call(UserSeeder::class);
-        $this->call(LocationSeeder::class);
+        // $this->call(LocationSeeder::class);
     }
 }
